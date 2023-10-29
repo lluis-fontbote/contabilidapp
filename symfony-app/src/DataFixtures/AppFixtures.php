d385321387b6eb1f0452bb6e03b89dd58814381a
Lluís Fontboté <lluis.fontbote@studiogenesis.es>
2023-10-29 22:57:44 +0100

    feat(providers): seed database with fake providers

--- symfony-app/src/DataFixtures/AppFixtures.php
+++ symfony-app/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,18 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\ProviderType;
+use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Provider;
+use App\Entity\ProviderType;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $this->loadProviderTypes($manager);
+        $this->loadProviders($manager);
     }
 
     private function loadProviderTypes(ObjectManager $manager): void
@@ -19,6 +22,28 @@ class AppFixtures extends Fixture
             $providerType = new ProviderType();
             $providerType->setName($providerTypeName);
             $manager->persist($providerType);
+        }
+
+        $manager->flush();
+    }
+
+    private function loadProviders(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        $providerTypes = $manager->getRepository(ProviderType::class)->findAll();
+
+        for ($i = 0; $i < 50; $i++) {
+            $provider = new Provider();
+            $provider->setName($faker->company);
+            $provider->setEmail($faker->email);
+            $provider->setPhone($faker->phoneNumber);
+            $provider->setActive($faker->boolean);
+
+            $randomProviderType = $faker->randomElement($providerTypes);
+            $provider->setProviderType($randomProviderType);
+
+            $manager->persist($provider);
         }
 
         $manager->flush();
